0c6cac4031c93987073aa9298527b9da9e9c2724
ong-wei-hong <weihong.ong@outlook.com>
2022-02-08 16:51:07 +0800

    Added Vets List and updated Controller. Closes #15

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -41,7 +41,7 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setId(1L);
         vet1.setFirstName("Sam");
-        vet1.setFirstName("Axe");
+        vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
