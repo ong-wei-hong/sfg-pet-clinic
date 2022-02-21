96c66a05973c3db38517ea1f55179c4836e4853c
ong-wei-hong <weihong.ong@outlook.com>
2022-02-21 17:02:57 +0800

    Created PetTypeService. Closes #29.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -2,7 +2,5 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
 public interface PetService extends CrudService<Pet, Long>{
 }
