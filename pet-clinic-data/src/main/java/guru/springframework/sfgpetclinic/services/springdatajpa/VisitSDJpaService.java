c591bbd549d697038aaa440f40d1457c529c29d9
ong-wei-hong <weihong.ong@outlook.com>
2022-02-23 16:42:40 +0800

    Created spring data jpa service for visit. Closes #47

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,45 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.repositories.VisitRepository;
+import guru.springframework.sfgpetclinic.services.VisitService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
